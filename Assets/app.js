db5218c0af8bf4b84d93c4c48fd7eec1f08c7bab
zdrale <nikolazd22@outlook.com>
2020-11-30 14:12:32 +0100

    Defined UI Variables

---
+++ Assets/app.js
@@ -0,0 +1,6 @@
+//Define UI Variables
+const form = document.getElementById('task-form');
+const taskList = document.getElementsByClassName('collection');
+const clearBtn = document.getElementsByClassName('clear-tasks');
+const filter = document.getElementsById('filter');
+const taskInput = document.getElementsById('task');
