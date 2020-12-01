44c915b82674ace7126dba3ea0ac61b327ae4959
zdrale <nikolazd22@outlook.com>
2020-12-01 11:16:23 +0100

    Added remove from local storage functionality
    --Tasks are now removed from local storage instead of the DOM.

--- Assets/app.js
+++ Assets/app.js
@@ -111,7 +111,28 @@ function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
     if(confirm('Are you sure?')) 
       e.target.parentElement.parentElement.remove();
+
+      //Remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
   }
+}
+
+//Remove Task From Local Storage
+function removeTaskFromLocalStorage (taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks')=== null) {
+    tasks=[];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //Clear Tasks
@@ -119,6 +140,14 @@ function clearTasks(e) {
   while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  //Clear from LS
+  clearTasksFromLocalStorage();
+}
+
+//Clear Tasks from local storage
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
 //Filter Tasks
